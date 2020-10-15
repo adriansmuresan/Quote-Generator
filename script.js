94afcf9c2519ec944459107cb2a3b8d7269907a6
Adrian S MUresan <adriansmuresan@gmail.com>
2020-10-15 14:42:48 -0500

    Declares variables, adds conditionals for the quote text, author, inserts data

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //  Get quote form API
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -5,10 +11,22 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    // If author is blank we set it to Unknown
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size fro long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote')
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
-    console.log('Whoops, no quote', error);
+
   }
 }
 
